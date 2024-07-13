7ac0f7f2b2533b9371dde161a0f86bfd23bfeaa6
saik434455@gmail.com <saik434455@gmail.com>
2024-07-13 15:15:28 +0530

    code refactor

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,20 +1,8 @@
 // src/routes/api.js
 const express = require('express');
 const router = express.Router();
+const ethController = require('../controllers/ethController');
 
-// Test route
-router.post('/process', (req, res) => {
-  const { data } = req.body;
-  
-  if (!data || typeof data !== 'string') {
-    return res.status(400).json({ error: 'Invalid input. Please provide a string in the "data" field.' });
-  }
-
-  // Example processing function
-  const processString = (str) => str.length;
-
-  const result = processString(data);
-  res.json({ result });
-});
+router.post('/process-eth-data', ethController.processEthData);
 
 module.exports = router;
--- src/test/test-api.js
+++ src/test/test-api.js
@@ -1,11 +1,65 @@
+// test.js
 const axios = require('axios');
 
-axios.post('http://localhost:3000/api/process', {
-  data: 'test string'
-})
-.then(function (response) {
-  console.log(response.data);
-})
-.catch(function (error) {
-  console.log(error);
-});
+const baseURL = 'http://localhost:3000/api';
+
+async function testProcessEthData() {
+  try {
+    const response = await axios.post(`${baseURL}/process-eth-data`, {
+      address: '0xC2F20D5c81F5B4450aA9cE62638d0bB01DF1935a',
+      id: 'testId123',
+      inputString: 'Hello, Ethereum!'
+    });
+    console.log('Success Response:', response.data);
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// async function testMissingFields() {
+//   try {
+//     const response = await axios.post(`${baseURL}/process-eth-data`, {
+//       address: '0xC2F20D5c81F5B4450aA9cE62638d0bB01DF1935a',
+//       // missing id and inputString
+//     });
+//   } catch (error) {
+//     handleError(error);
+//   }
+// }
+
+// async function testInvalidAddress() {
+//   try {
+//     const response = await axios.post(`${baseURL}/process-eth-data`, {
+//       address: 'invalidAddress',
+//       id: 'testId123',
+//       inputString: 'Hello, Ethereum!'
+//     });
+//   } catch (error) {
+//     handleError(error);
+//   }
+// }
+
+function handleError(error) {
+  if (error.response) {
+    console.log("Error from test-api.js:");
+    console.log('Error data:', error.response.data);
+    console.log('Error status:', error.response.status);
+  } else if (error.request) {
+    console.log('Error request:', error.request);
+  } else {
+    console.log('Error message:', error.message);
+  }
+}
+
+// async function runTests() {
+//   console.log('Testing process-eth-data with valid input:');
+//   await testProcessEthData();
+  
+//   console.log('\nTesting process-eth-data with missing fields:');
+//   await testMissingFields();
+  
+//   console.log('\nTesting process-eth-data with invalid address:');
+//   await testInvalidAddress();
+// }
+testProcessEthData()
+// runTests();
