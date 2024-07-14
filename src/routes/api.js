97700c6c7ea7ee6049cb5c29df09af5328d83dcd
saik434455@gmail.com <saik434455@gmail.com>
2024-07-14 20:24:31 +0530

    verify proof api

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const ethController = require('../controllers/ethController');
 const zokratesController = require('../controllers/zokratesController');
-
+const verificationController = require('../controllers/verificationController');
 router.post('/process-eth-data', ethController.processEthData);
 router.post('/run-zokrates', zokratesController.runZokrates)
+router.post('/verify-proof',verificationController.verifyZKProof)
 module.exports = router;
