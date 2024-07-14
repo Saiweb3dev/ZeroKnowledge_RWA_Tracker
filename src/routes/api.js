6a65ad8f8ce55da6d0b3f0aa956b5a7a67d68cea
saik434455@gmail.com <saik434455@gmail.com>
2024-07-14 19:32:26 +0530

    POST method api for calling zok

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const ethController = require('../controllers/ethController');
+const zokratesController = require('../controllers/zokratesController');
 
 router.post('/process-eth-data', ethController.processEthData);
-
+router.post('/run-zokrates', zokratesController.runZokrates)
 module.exports = router;
