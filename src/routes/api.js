f61cdb01f23b03fd5e2a8329e252da0a452569e9
saik434455@gmail.com <saik434455@gmail.com>
2024-07-17 20:49:23 +0530

    beta api endpoint testing giles

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const ethController = require('../controllers/ethController');
 const zokratesController = require('../controllers/zokratesController');
 const verificationController = require('../controllers/verificationController');
+const zokratesControllerBeta = require('../controllers/zokratesControllerBeta');
 router.post('/process-eth-data', ethController.processEthData);
 router.post('/run-zokrates', zokratesController.runZokrates)
 router.post('/verify-proof',verificationController.verifyZKProof)
+
+router.post('/run-zokrates-beta',zokratesControllerBeta.runZokratesBeta)
 module.exports = router;
