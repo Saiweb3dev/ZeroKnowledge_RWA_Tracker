2c13b1b1853ab9632b9e94d8ae2d48112efba4bb
saik434455@gmail.com <saik434455@gmail.com>
2024-08-03 15:55:06 +0530

    .

--- src/config/ethConfig.js
+++ src/config/ethConfig.js
@@ -1,11 +1,11 @@
 const {ethers} = require('ethers')
 const contractData = require('../../deployments/hardhat/RWA_Tokenizer.json');
-
+import { JsonRpcProvider } from 'ethers';
 const contractABI = contractData.abi;
 const contractAddress = contractData.address;
 const SEPOLIA_RPC_URL ="https://eth-sepolia.g.alchemy.com/v2/8cAuHYtk5pZaV5S4z9QKIKLbAj3JEc3i";
 // Connect to local Hardhat network
-const provider = new ethers.JsonRpcProvider(SEPOLIA_RPC_URL);
+const provider = new JsonRpcProvider(SEPOLIA_RPC_URL);
 
 // Create a contract instance
 const contract = new ethers.Contract(contractAddress, contractABI, provider)
